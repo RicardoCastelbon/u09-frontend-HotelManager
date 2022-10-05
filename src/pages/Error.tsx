8ea20bc9f7a44174f924c1e7a1767c5eeb4b8de8
Ricardo Castelbon <ricardocastelbon@gmail.com>
2022-10-05 13:08:45 +0200

    add not found page

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,6 +1,19 @@
+import { Link } from "react-router-dom";
+import img from "../assets/images/not-found.svg";
+import Wrapper from "../assets/wrappers/ErrorPage";
+
 type Props = {};
 const Error = (props: Props) => {
-  return <div>Error</div>;
+  return (
+    <Wrapper className="full-page">
+      <div>
+        <img src={img} alt="not found" />
+        <h3>Ohh! Page not found</h3>
+        <p>We can't find the page you are looking for</p>
+        <Link to="/">back home</Link>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Error;
