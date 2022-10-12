d42766ed5a296cc7ff80488f3bbc3557d72325d3
Ricardo Castelbon <ricardocastelbon@gmail.com>
2022-10-12 21:39:04 +0200

    add sidebar modal

--- src/components/SmallSidebar.tsx
+++ src/components/SmallSidebar.tsx
@@ -1,10 +1,47 @@
+import { FaTimes } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
 import Wrapper from "../assets/wrappers/SmallSidebar";
+import { useAppContext } from "../context/appContext";
+import links from "../utils/links";
+import Logo from "./Logo";
 
 type Props = {};
 const SmallSidebar = (props: Props) => {
+  const { showSidebar, toggleSidebar } = useAppContext();
   return (
     <Wrapper>
-      <div>SmallSidebar</div>;
+      <div
+        className={
+          showSidebar ? "sidebar-container show-sidebar" : "sidebar-container"
+        }
+      >
+        <div className="content">
+          <button type="button" className="close-btn" onClick={toggleSidebar}>
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <div className="nav-links">
+            {links.map((link) => {
+              const { text, path, id, icon } = link;
+              return (
+                <NavLink
+                  to={path}
+                  key={id}
+                  onClick={toggleSidebar}
+                  className={({ isActive }) =>
+                    isActive ? "nav-link" : "nav-link"
+                  }
+                >
+                  <span className="icon">{icon}</span>
+                  {text}
+                </NavLink>
+              );
+            })}
+          </div>
+        </div>
+      </div>
     </Wrapper>
   );
 };
---
+++ src/utils/links.tsx
@@ -0,0 +1,18 @@
+import { RiBookletLine } from "react-icons/ri";
+import { IoAddCircleOutline } from "react-icons/io5";
+import { AiOutlineSearch } from "react-icons/ai";
+import { FiSettings } from "react-icons/fi";
+
+const links = [
+  { id: 1, text: "bookings", path: "/", icon: <RiBookletLine /> },
+  {
+    id: 2,
+    text: "add booking",
+    path: "add-booking",
+    icon: <IoAddCircleOutline />,
+  },
+  { id: 3, text: "search booking", path: "search-booking", icon: <AiOutlineSearch /> },
+  { id: 4, text: "settings", path: "settings", icon: <FiSettings /> },
+];
+
+export default links;
